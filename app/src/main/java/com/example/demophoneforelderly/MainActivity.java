4ee9900a6a39793cc6b2bc9a40646efd91c70367
ZINALEEPOH <22031511@myrp.edu.sg>
2023-05-30 23:10:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demophoneforelderly/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.demophoneforelderly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnS, btnD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnS=findViewById(R.id.button);
+        btnD=findViewById(R.id.button2);
+
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+92240336));
+                startActivity(intentCall);
+            }
+        });
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+89675543));
+                startActivity(intentCall);
+            }
+        });
+    }
+
+}
